a36cdbc6ade392b4241cc36d3a2cffe64ea07a1d
xuzewei <xuzewei_2012@126.com>
2017-04-05 13:41:12 +0800

    Version

---
+++ src/main/java/com/github/xuzw/memory/cli/cmd/Version.java
@@ -0,0 +1,13 @@
+package com.github.xuzw.memory.cli.cmd;
+
+/**
+ * @author 徐泽威 xuzewei_2012@126.com
+ * @time 2017年4月5日 下午1:37:06
+ */
+public class Version {
+    private static final String version = "2017.04.05.1339";
+
+    public void execute() {
+        System.out.println(version);
+    }
+}
